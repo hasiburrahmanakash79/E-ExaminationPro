76d5255743461dd9b8c84b070ab1d1cb3bd05cac
Abir-07 <md.tazwarul.islam.07@gmail.com>
2023-08-17 11:19:53 +0600

    EX-26 <progressbar with time >

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,11 @@ import router from "./Routes/Router";
 import AuthProvider from "./Provider/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+<div className="navigation-bar">
+<React.StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
   </React.StrictMode>
+</div>
 );
