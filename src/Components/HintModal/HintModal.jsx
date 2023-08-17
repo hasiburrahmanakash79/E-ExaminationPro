6d3039c5664b2a2fe13c5211d64d65e46ffeb5c3
habib-N19 <habiburnabiarafat@gmail.com>
2023-08-18 04:19:57 +0600

    merged with ex-20

--- src/Components/HintModal/HintModal.jsx
+++ src/Components/HintModal/HintModal.jsx
@@ -1,16 +1,15 @@
-
+/* eslint-disable react/prop-types */
 
 const HintModal = ({ question, hint, isModalOpen, onClose }) => {
   return (
     <>
       {isModalOpen && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center">
-          <div className="modal modal-open">
-            <div className="modal-box">
-              
-              <h2 className="text-lg font-semibold mb-2">Hint</h2>
-              <p className="mb-4">{hint}</p>
-              <button className="btn" onClick={onClose}>
+        <div className='fixed inset-0 z-50 flex items-center justify-center'>
+          <div className='modal modal-open'>
+            <div className='modal-box'>
+              <h2 className='mb-2 text-lg font-semibold'>Hint</h2>
+              <p className='mb-4'>{hint}</p>
+              <button className='btn' onClick={onClose}>
                 Close
               </button>
             </div>
@@ -18,7 +17,7 @@ const HintModal = ({ question, hint, isModalOpen, onClose }) => {
         </div>
       )}
     </>
-  );
-};
+  )
+}
 
-export default HintModal;
+export default HintModal
