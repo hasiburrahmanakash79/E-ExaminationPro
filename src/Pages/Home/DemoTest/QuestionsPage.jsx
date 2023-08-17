732d8feaf0b8c15dfa56257663d55be661bb6c0a
habib-N19 <habiburnabiarafat@gmail.com>
2023-08-18 01:03:05 +0600

    EX-16 <added a template where user can see in which question they are in, missing stylesgit add .>

--- src/Pages/Home/DemoTest/QuestionsPage.jsx
+++ src/Pages/Home/DemoTest/QuestionsPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
 
 const QuestionsPage = ({ question, selectedOption, onAnswerSelected }) => {
   const handleRadioChange = choiceId => {
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./Routes/Router";
-import AuthProvider from "./Provider/AuthProvider";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './Routes/Router'
+import AuthProvider from './Provider/AuthProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const queryClient = new QueryClient()
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>
-);
+)
