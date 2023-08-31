fae90df8a882ca94f32fdf71c69d5fe6e79f11a9
habib-N19 <habiburnabiarafat@gmail.com>
2023-08-31 21:55:11 +0600

    EX-53 finally an working short question section with redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 import demoExamSlice from "./features/demoExam/demoExamSlice";
+import shortQuestionSlice from "./features/shortQuestion/shortQuestionSlice";
 
 
 const store = configureStore({
     reducer: {
-        demoExam: demoExamSlice
+        demoExam: demoExamSlice,
+        shortQuestions: shortQuestionSlice
     }
 })
 
