ff493f086377f07f2c7c641fd877854e734841a3
saifularafat <saifularafat.info@gmail.com>
2023-09-05 02:14:59 +0600

    EX-59 error fixed

--- src/Hooks/useAxiosSecure.jsx/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx/useAxiosSecure.jsx
@@ -5,11 +5,7 @@ import { AuthContext } from '../../Provider/AuthProvider'
 
 const axiosSecure = axios.create({
  // baseURL: 'https://e-exam-pro-server.vercel.app'
-<<<<<<< HEAD
-  baseURL: 'http://localhost:5000'
-=======
   baseURL: 'http://localhost:5000/'
->>>>>>> 0878a28c16f3c1c3afdb41c4211a3c42d594e4ce
 })
 
 const useAxiosSecure = () => {
